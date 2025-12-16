849945b09f8f281e1f28a88a01380138ece3fe60
chandu <chandugit68@gmail.com>
2025-12-16 22:51:58 +0530

    rsvp controllers are added

--- src/controllers/event.controller.js
+++ src/controllers/event.controller.js
@@ -37,7 +37,7 @@ const createEvent = async (req, res) => {
 
 const getAllEvents = async (req, res) => {
     try {
-        const events = await Event.find().sort({ dateTime: 1 }).populate("createdBy", "email");
+        const events = await Event.find().sort({ dateTime: 1 }).populate("createdBy","email userName")
         if (events)
             return res.status(200).json({ events });
         return res.status(404).json({ message: "No events found for you" });
@@ -92,4 +92,60 @@ const deleteEvent = async (req, res) => {
     }
 }
 
-module.exports = { createEvent, getAllEvents, updateEvent, deleteEvent };
+const joinEvent=async(req,res)=>{
+    const id=req.params.id;
+    const getEvent=await Event.findById(id);
+    try{
+        const event = await Event.findOneAndUpdate(
+            {
+                _id:id,
+                currentCount:{$lt:getEvent.capacity},
+                attendees:{$ne:req.user.userId}
+            },
+            {
+                $inc:{currentCount:1},
+                $addToSet:{attendees:req.user.userId}
+            },
+            {new:true}
+        );
+        if(!event)
+            return res.status(400).json({message:"Event is full or already joined"});
+        res.status(200).json({
+            message:"Event joined successfull",
+            event:event
+        });
+    }catch(err){
+        console.log(err);
+        res.status.json({
+            message:"Internal Error"
+        })
+    }
+}
+
+const leaveEvent=async(req,res)=>{
+    const id=req.params.id;
+    try{
+        const event=await Event.findOneAndUpdate(
+            {
+                _id:id,
+                attendees:req.user.userId
+            },
+            {
+                $pull:{attendees:req.user.userId},
+                $inc:{currentCount:-1}
+            },
+            {new:true}
+        );
+        if(!event)
+            return res.status(400).json({message:"You are not of this event"});
+        res.status(200).json({
+            message:"You are leaved this event successfully",
+            leavedEvent:event
+        });
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message:"Internal error"});
+    }
+}
+
+module.exports = { createEvent, getAllEvents, updateEvent, deleteEvent,joinEvent,leaveEvent };
--- src/routes/event.routes.js
+++ src/routes/event.routes.js
@@ -1,12 +1,13 @@
 const express=require('express')
 const eventRouter=express.Router();
 const auth=require('../middlewares/auth.middleware');
-const { createEvent, getAllEvents, updateEvent, deleteEvent }=require('../controllers/event.controller')
+const { createEvent, getAllEvents, updateEvent, deleteEvent,joinEvent,leaveEvent }=require('../controllers/event.controller')
 
 eventRouter.post('/create/event',auth,createEvent)
 eventRouter.get('/get/events',getAllEvents)
 eventRouter.patch('/update/event/:id',auth,updateEvent)
 eventRouter.delete('/delete/event/:id',auth,deleteEvent)
-
+eventRouter.post('/join/event/:id',auth,joinEvent);
+eventRouter.post('/leave/event/:id',auth,leaveEvent);
 
 module.exports=eventRouter;
